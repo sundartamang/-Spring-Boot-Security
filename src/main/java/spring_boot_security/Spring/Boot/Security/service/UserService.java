a56afd66fca68369e7f04396c0c552fec42a011e
sursasundar <sundar.tamang@sursatech.com>
2025-01-02 16:18:07 +0545

    feat: use authenticationManager layer, added user-login endpoint, generate token and return it if user if authenticaticated

--- src/main/java/spring_boot_security/Spring/Boot/Security/service/UserService.java
+++ src/main/java/spring_boot_security/Spring/Boot/Security/service/UserService.java
@@ -1,6 +1,9 @@
 package spring_boot_security.Spring.Boot.Security.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import spring_boot_security.Spring.Boot.Security.model.Users;
@@ -10,12 +13,26 @@ import spring_boot_security.Spring.Boot.Security.repo.UserRepo;
 public class UserService {
 
     @Autowired
-    UserRepo userRepo;
+    private UserRepo userRepo;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public Users register(Users user){
         user.setPassword(encoder.encode(user.getPassword()));
         return  userRepo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication auth = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(auth.isAuthenticated())
+            return jwtService.generateToken(user.getUsername());
+        return  "Fail";
     }
 }
